9ad0d7578c469510843bb557883c353054ddf5fc
Damir <78542749+Mrkeroch15@users.noreply.github.com>
2021-12-30 02:48:48 +0600

    Update main.c

--- Objective-C Arrays task 3/main.c
+++ Objective-C Arrays task 3/main.c
@@ -19,14 +19,12 @@ int main() {
     printf("\n");
     for (int i = 0; i < N; ++i) {
       if (A[i] % 2 == 0) {
-        printf("%d", A[i]);
-        printf(" ");
+        printf("%d ", A[i]);
       }
     }
     for (int i = 0; i < N; ++i) {
       if (A[i] % 2 != 0) {
-        printf("%d", A[i]);
-        printf(" ");
+        printf("%d ", A[i]);
       }
     }
 
